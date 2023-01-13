457f65e5dd2344b8436369f08fbe62a177b9a5e0
Indika Gunawardana <indika.gunawardana@gmail.com>
2023-01-13 22:39:14 +0530

    Implemented beautify and minify methods.

--- js/ace-config.js
+++ js/ace-config.js
@@ -1,28 +1,49 @@
 
 let editor;
+let beautify;
 (
     function () {
         editor = ace.edit("editor");
+        beautify = ace.require("ace/ext/beautify");
         editor.setTheme("ace/theme/crimson_editor");
         editor.session.setMode("ace/mode/json");
+        editor.session.setUseWrapMode(true);
         console.log('initializing the ace')
         //getDefaultData("L")
 
     }
 )();
 
-editor.session.on('change', function(delta) {
+editor.session.on('change', function (delta) {
     // delta.start, delta.end, delta.lines, delta.action
     //console.log(delta);
-    console.log(editor.getValue());
+    //console.log(editor.getValue());
 });
+
+$("#format").click(function () {
+    format();
+});
+
+$("#minify").click(function () {
+    minify();
+});
+
+/**
+ * Remove all white spaces from the json.
+ */
+function minify() {
+    let originalValue = editor.getValue();
+    let minified = originalValue.replaceAll(' ', '').replaceAll('\n', '').replaceAll('\t', '');
+    editor.setValue(minified);
+}
 
 /**
  * Format JSON and set value.
  */
 function format() {
-    let originalValue = editor.getValue();
-    editor.setValue(JSON.stringify(originalValue, null, 4));
+    //let originalValue = editor.getValue();
+    //editor.setValue(JSON.stringify(originalValue, null, 4));
+    beautify.beautify(editor.session);
 }
 
 function getDefaultData(type) {
