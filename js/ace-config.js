2cb70ea815d7e1c36f15eade0363539d3f822144
Indika Gunawardana <indika.gunawardana@gmail.com>
2023-01-14 05:46:49 +0530

    Added dynamic editor buttons

--- js/ace-config.js
+++ js/ace-config.js
@@ -1,6 +1,13 @@
 
+//global ace editor instance
 let editor;
+//global ace editor extension for beautify
 let beautify;
+//editor id list
+let editorList = [];
+//current activated editor
+let currentEditor;
+
 (
     function () {
         editor = ace.edit("editor");
@@ -10,7 +17,11 @@ let beautify;
         editor.session.setUseWrapMode(true);
         console.log('initializing the ace')
         //getDefaultData("L")
+        //Initial editor id.
+        editorList.push("sheet1");
 
+        //initialize current editor to the initial editor.
+        currentEditor = editorList[0];
     }
 )();
 
@@ -20,10 +31,39 @@ editor.session.on('change', function (delta) {
     //console.log(editor.getValue());
 });
 
+/**
+ * Add editor button click
+ */
+$("#addEditor").click(function () {
+    if (editorList.length >= 5) {
+        console.log('Max allowed editor count is 5')
+        return;
+    }
+    console.log('add button');
+    var id = 'sheet' + (editorList.length + 1);
+    var r = $('<button type="button" class="editor-button btn btn-success" id="' + id + '">Sheet-' + (editorList.length + 1) + '</button>');
+    editorList.push(id);
+
+    $("#editorList").append(r);
+});
+
+$(document).on('click', '.editor-button', function ($event) {
+    alert(this.id);
+    //todo
+    //assign ace editor session to each button
+    //load content based on the selected editor id
+});
+
+/**
+ * Format button click event.
+ */
 $("#format").click(function () {
     format();
 });
 
+/**
+ * Minify button click event.
+ */
 $("#minify").click(function () {
     minify();
 });
