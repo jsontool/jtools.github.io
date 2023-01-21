10ab84fb846eed9c38c0b0c15297a11cfbfd2625
Indika Gunawardana <indika.gunawardana@gmail.com>
2023-01-21 13:56:12 +0530

    Style changes

--- js/ace-config.js
+++ js/ace-config.js
@@ -44,7 +44,7 @@ $("#addEditor").click(function () {
     }
     console.log('add button');
     var id = editorList.length;
-    var r = $('<button type="button" class="pure-button editor-button" id="' + id + '">Tab - ' + (editorList.length + 1) + '</button>');
+    var r = $('<button type="button" class="button-59 editor-button" id="' + id + '">Tab - ' + (editorList.length + 1) + '</button>');
     editorList.push(id);
 
     $("#editorList").append(r);
